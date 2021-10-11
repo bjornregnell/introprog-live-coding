37c9cc79230017aa3dcb1f830fa51a0f38e10a82
bjornregnell <bjorn.regnell@cs.lth.se>
2021-10-11 14:24:30 +0200

    add example slide 22

---
+++ w07-sequences/22-övn-copy-filter.scala
@@ -0,0 +1,9 @@
+def copy(xs: Vector[Int]): Vector[Int] = ???
+
+def filter(xs: Vector[Int], p: Int => Boolean): Vector[Int] = ???
+
+def findIndices(xs: Vector[Int], p: Int => Boolean): Vector[Int] = ???
+
+def sort(xs: Vector[Int]): Vector[Int] = ???
+
+def freq(xs: Vector[Int]): Vector[(Int, Int)] = ???  // (heltal, frekvens)
